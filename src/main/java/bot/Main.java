214bb62a7ea7853b26f843978a5db87ea52a7e05
KavyanNasseri <77510794+KavyanNasseri@users.noreply.github.com>
2022-03-09 00:18:12 -0500

    NO NEW TOKENS ANYMORE!!

--- src/main/java/bot/Main.java
+++ src/main/java/bot/Main.java
@@ -4,12 +4,24 @@ import bot.events.Events;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import org.json.JSONObject;
 
 import javax.security.auth.login.LoginException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws LoginException {
-        JDA bot = JDABuilder.createDefault("OTQ5MTI3MjUwMzM3NDY4NDQ2.YiF1og.HSE1IKFjdHwI6HCSpPIdOzf3-gI").setActivity(Activity.playing("With my balls")).build();
+    public static void main(String[] args) throws LoginException, IOException {
+        BufferedReader br = new BufferedReader(new FileReader("D:\\New Bot\\config.json"));
+        String line;
+        StringBuilder sbObj = new StringBuilder();
+        while((line = br.readLine()) != null){
+            sbObj.append(line);
+        }
+        JSONObject obj = new JSONObject(sbObj.toString());
+        JDA bot = JDABuilder.createDefault(obj.getString("token")).setActivity(Activity.playing("With my balls")).build();
         bot.addEventListener(new Events());
     }
 }
